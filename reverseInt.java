4e4b6654a877fe34a7bc15c45cdcf05a4a46f05b
sonisomitra <sonisomitra1@gmail.com>
2026-06-02 20:34:13 +0530

    Create reverseInt.java

---
+++ reverseInt.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        int result=0;
+        while (x!=0){
+        int digit = x % 10;
+        if(result > Integer.MAX_VALUE /10 || result==Integer.MAX_VALUE / 10 && digit > 7){
+            return 0;
+        }
+        if(result < Integer.MIN_VALUE /10 || result==Integer.MIN_VALUE / 10 && digit < -8){
+            return 0;
+        }
+        result = result * 10 + digit;
+        x = x/10;
+        }
+    return result;
+    }
+public static void main(String[] args){
+  Solution obj1 = new Solution();
+  obj1.reverse(123345456);
+
+}
