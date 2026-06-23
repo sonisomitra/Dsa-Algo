4c9c212ec78b51824db1410afc95266a79d0acec
sonisomitra <sonisomitra1@gmail.com>
2026-06-23 23:05:49 +0530

    Create HouseRobber.java

---
+++ HouseRobber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] nums) {
+        int rob1=0;
+        int rob2=0;
+            for(int i:nums){
+            int temp = Math.max(i+rob1,rob2);
+            rob1=rob2;
+            rob2=temp;
+            }
+        return rob2;
+    }
+}
