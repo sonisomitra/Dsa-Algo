52f6c737416f7870e80e159e21596d1d3b761230
sonisomitra <sonisomitra1@gmail.com>
2026-08-22 17:51:36 +0000

    Create (338)CountingBits.java

---
+++ (338)CountingBits.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] countBits(int n) {
+    int[]  bits = new int[n+1];
+    for (int i = 1; i <= n; i++) {
+        bits[i] = bits[i & (i - 1)] + 1;
+    }
+    return bits;   
+    }
+}
